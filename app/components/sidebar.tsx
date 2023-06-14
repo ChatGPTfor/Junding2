d78e867b6796b4ff1a69e8cb45c5454016c20174
Menethl <wanjiang0100@126.com>
2023-06-15 03:06:11 +0800

    Update sidebar.tsx

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -119,9 +119,9 @@ export function SideBar(props: { className?: string }) {
       }`}
     >
       <div className={styles["sidebar-header"]}>
-        <div className={styles["sidebar-title"]}>小青龙AI</div>
+        <div className={styles["sidebar-title"]}>珺顶AI</div>
         <div className={styles["sidebar-sub-title"]}>
-          专注AI应用 赋能未来.
+          珺顶AI 赋能直播未来.
         </div>
         <div className={styles["sidebar-logo"] + " no-dark"}>
           <ChatGptIcon />
